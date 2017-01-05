9395219c71a6d2174b9e462b50b4fbcdd280037c
Jake Bush <jackburton@Jakes-MacBook-Air.local>
2017-01-05 13:55:59 -0800

    Added styling, audio and custom font

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Jake Bush on 1/5/17.
+//  Copyright © 2017 PorkChopExpress. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
